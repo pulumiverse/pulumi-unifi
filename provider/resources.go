80f0ab61f33d18b587acb30aa1e1a244951ae2ef
Ringo De Smet <ringo@de-smet.name>
2022-03-17 22:27:04 +0100

    Updated the NPM package name
    Signed-off-by: Ringo De Smet <ringo@de-smet.name>

--- provider/resources.go
+++ provider/resources.go
@@ -138,6 +138,7 @@ func Provider() tfbridge.ProviderInfo {
 			"unifi_user_group":     {Tok: tfbridge.MakeDataSource(mainPkg, mainMod, "getUserGroup")},
 		},
 		JavaScript: &tfbridge.JavaScriptInfo{
+			PackageName: "@pulumiverse/unifi",
 			// List any npm dependencies and their versions
 			Dependencies: map[string]string{
 				"@pulumi/pulumi": "^3.0.0",
