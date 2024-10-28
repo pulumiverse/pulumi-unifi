4176ec3518f3b17a0f91b6d0fb298fda1ec78cd9
Ringo De Smet <ringo@de-smet.name>
2024-10-28 16:38:31 +0100

    Change schema embedding to fix Go linting

--- provider/cmd/pulumi-resource-unifi/main.go
+++ provider/cmd/pulumi-resource-unifi/main.go
@@ -17,11 +17,16 @@
 package main
 
 import (
+	_ "embed"
+
 	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfbridge"
 
 	unifi "github.com/pulumiverse/pulumi-unifi/provider"
 	"github.com/pulumiverse/pulumi-unifi/provider/pkg/version"
 )
+
+//go:embed schema-embed.json
+var pulumiSchema []byte
 
 func main() {
 	// Modify the path to point to the new provider
